6804f691a0081fc0f2f10ac10b4e2d328e47ae00
FelipeBlancato <felipeblancato04@gmail.com>
2026-02-24 17:45:38 -0300

    Mov 0.1

--- cobrinha.py
+++ cobrinha.py
@@ -1,22 +1,23 @@
 import pygame
 from pygame.locals import *
 
-JANELA_WIDTH = 600
-JANELA_HEIGHT = 600
+JANELA_WIDTH = 900
+JANELA_HEIGHT = 900
 BLOCK = 20
-POS_INICIAL_X = 290
-POS_INICIAL_Y = 290
+POS_INICIAL_X = JANELA_WIDTH // 2 - BLOCK // 2
+POS_INICIAL_Y = JANELA_HEIGHT // 2 - BLOCK // 2
 
 pygame.init()
 
 janela = pygame.display.set_mode((JANELA_WIDTH, JANELA_HEIGHT))
-janela.fill((255,255,204))
 
 cobra_surface = pygame.Surface((BLOCK, BLOCK))
 cobra_pos = [(POS_INICIAL_X, POS_INICIAL_Y)]
 cobra_surface.fill((68,189,50))
 
 while True:
+    pygame.time.Clock().tick(10)
+    janela.fill((255,255,204))
 
     for evento in pygame.event.get():
         if evento.type == QUIT:
@@ -25,5 +26,10 @@ while True:
         
     for pos in cobra_pos:
         janela.blit(cobra_surface,pos)
+
+    cobra_pos[0] = cobra_pos[0][0] + BLOCK, cobra_pos[0][1]
+    #cobra_pos[0] = cobra_pos[0][0] - BLOCK, cobra_pos[0][1]
+    cobra_pos[0] = cobra_pos[0][0], cobra_pos[0][1] - BLOCK
+    #cobra_pos[0] = cobra_pos[0][0], cobra_pos[0][1] + BLOCK
 
     pygame.display.update()
